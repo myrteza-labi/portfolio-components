687dd564134d594b6f4c350a2b8a4be7c469c78e
Myrteza <myrteza@hotmail.fr>
2021-11-07 18:02:31 +0100

    :main: finish the Parallax Component

--- src/components/parallax/Parallax.js
+++ src/components/parallax/Parallax.js
@@ -8,8 +8,38 @@ class Parallax extends React.Component {
     constructor(props){
         super(props); 
         this.state = {
-
+            activeText : [
+                " ", 
+                " ", 
+                " ", 
+                " ", 
+                " ",                                
+            ],
+            liActivePosition : [
+                {
+                    turnOn : 890,
+                    turnOff : 900,
+                },
+                {
+                    turnOn : 900,
+                    turnOff : 715,
+                },
+                {
+                    turnOn : 715,
+                    turnOff : 600,
+                },
+                {
+                    turnOn : 600,
+                    turnOff : 450,
+                },
+                {
+                    turnOn : 450,
+                    turnOff : 600,
+                },
+            ],
         }
+        this.handleClick = this.handleClick.bind(this); 
+        this.handleScroll = this.handleScroll.bind(this); 
     }
 
     componentDidMount() {
@@ -21,22 +51,100 @@ class Parallax extends React.Component {
     }
     
     handleScroll(e) {
-        alert('scroll')
+
+
+        const newArray = this.state.activeText.slice(); 
+
+        const innerHeight = window.innerHeight; 
+
+        const li1posY = document.getElementById('li1').getBoundingClientRect().y * 100 / innerHeight; 
+        const li2posY = document.getElementById('li2').getBoundingClientRect().y * 100 / innerHeight; 
+        const li3posY = document.getElementById('li3').getBoundingClientRect().y * 100 / innerHeight; 
+        const li4posY = document.getElementById('li4').getBoundingClientRect().y * 100 / innerHeight; 
+        const li5posY = document.getElementById('li5').getBoundingClientRect().y * 100 / innerHeight; 
+
+
+        console.log(li1posY); 
+
+
+        
+         
+
+
+        /* li 1 */
+        if(li1posY >= 70){
+            newArray[0] = "active ";
+            newArray[1] = " ";             
+            this.setState({
+                activeText : newArray, 
+            })
+        }
+
+        if (li1posY >= 50 && li1posY < 70){
+            newArray[0] = " "; 
+            newArray[1] = "active "; 
+            newArray[2] = " "; 
+            this.setState({
+                activeText : newArray, 
+            })
+        }
+
+        if (li1posY >= 30 && li1posY < 50){
+            newArray[1] = " "; 
+            newArray[2] = "active "; 
+            newArray[3] = " "; 
+            this.setState({
+                activeText : newArray, 
+            })
+        }
+        if (li1posY >= 10 && li1posY < 30){
+            newArray[2] = " "; 
+            newArray[3] = "active "; 
+            newArray[4] = " "; 
+            this.setState({
+                activeText : newArray, 
+            })
+        }
+        if (li1posY >= 0 && li1posY < 10){
+            newArray[3] = " "; 
+            newArray[4] = "active "; 
+            this.setState({
+                activeText : newArray, 
+            })
+        }
+
+
+        
+        
+
+        
+
+        /*
+        const li1Rect = this.state.li1.getBoundingClientRect(); 
+        const li1posY = li1Rect.y; 
+        console.log(li1posY); 
+        */
     }
+    
 
     handleClick(e){
-        let current = e.target.getBoundingClientRect()
-        e.
-        alert(current.y)
+        const li1 = document.getElementById('li1');
+        /*
+        const li1Rect = li1.getBoundingClientRect(); 
+        const li1posY = li1Rect.y; */
+        console.log(li1.id); 
     }
 
+    
+
     render(){
+
         return (
             <div className="Parallax">
                 <div className="parallaxMainCtn">
                     <ParallaxBg/>
                 </div>
-                <ParallaxText onClick={this.handleClick}/>
+                <ParallaxText activeTextArray={this.state.activeText} onClick={this.handleClick}/>
             </div>
         )
     }
--- src/components/parallax/ParallaxText.js
+++ src/components/parallax/ParallaxText.js
@@ -1,38 +1,82 @@
+import { render } from '@testing-library/react';
 import React from 'react'
 import './ParallaxText.css'; 
 
-function ParallaxText(props) {
 
-     
-    
-    return (
+class ParallaxText extends React.Component {
+    constructor(props){
+        super(props); 
+        this.state = {
+            activeText : [
+                "active ", 
+                " ", 
+                " ", 
+                " ", 
+                " ",                                
+            ],
+            liActivePosition : [
+                {
+                    turnOn : 890,
+                    turnOff : 900,
+                },
+                {
+                    turnOn : 900,
+                    turnOff : 715,
+                },
+                {
+                    turnOn : 715,
+                    turnOff : 600,
+                },
+                {
+                    turnOn : 600,
+                    turnOff : 450,
+                },
+                {
+                    turnOn : 450,
+                    turnOff : 600,
+                },
+            ],
+            liCurrentPosition : [
+                " ",
+                " ",
+                " ",
+                " ",
+                " ",
+            ],
+        }
+    }
+
+
+    render(){
+
+        const activeTextArray = this.props.activeTextArray; 
+
+        return (
             <div  className="ParallaxText">
 
                 <ul  className="liList">
 
-                    <li value={2} onClick={props.onClick} className={"phrase " + props["data-active"]}>
-                    Un système photo nettement plus puissant.
-                    </li>
+                    <li id="li1" className={"phrase " + activeTextArray[0]}>
+                    Un système photo nettement plus puissant. </li>
 
-                    <li onClick={props.onClick} className={"phrase " + props["data-active"]}>
-                    Un écran si réactif qu’il renouvelle vos sensations à chaque interaction.   
-                    </li>
+                    <li id="li2" className={"phrase " + activeTextArray[1]}>
+                    Un écran si réactif qu’il renouvelle vos sensations à chaque interaction. </li>
 
-                    <li onClick={props.onClick} className={"phrase " + props["data-active"]}>
-                    La puce de smartphone la plus rapide au monde.
-                    </li>
+                    <li id="li3"  className={"phrase " + activeTextArray[2]}>
+                    La puce de smartphone la plus rapide au monde. </li>
 
-                    <li onClick={props.onClick} className={"phrase " + props["data-active"]}>
-                    Une résistance exceptionnelle.   
-                    </li>
+                    <li id="li4"  className={"phrase " + activeTextArray[3]}>
+                    Une résistance exceptionnelle. </li>
 
-                    <li onClick={props.onClick} className={"phrase " + props["data-active"]}>
-                    Et un gain d’autonomie spectaculaire.
-                    </li>
+                    <li id="li5"  className={"phrase " + activeTextArray[4]}>
+                    Et un gain d’autonomie spectaculaire. </li>
 
                 </ul>
             </div>
     )
+    }
+    
+    
 }
 
 export default ParallaxText
